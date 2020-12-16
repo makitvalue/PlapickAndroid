e9239d2d3c6c9e4d8699b1be1d53a947b25ad82a
jaehyunpark1995 <jaehyun959595@gmail.com>
2020-12-17 01:11:02 +0900

    ??

---
+++ app/src/main/java/com/example/plapick/ui/home/HomeFragment.kt
@@ -0,0 +1,41 @@
+package com.example.plapick.ui.home
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.example.plapick.R
+
+class HomeFragment : Fragment() {
+
+    private lateinit var homeViewModel: HomeViewModel
+
+    private val TAG = "태그"
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel =
+                ViewModelProvider(this).get(HomeViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        val swipe: SwipeRefreshLayout = root.findViewById(R.id.sl_swipe_home)
+
+        swipe.setOnRefreshListener {
+            Log.d(TAG, "SWIPE!!")
+            swipe.isRefreshing = false
+        }
+
+//        val textView: TextView = root.findViewById(R.id.text_home)
+//        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
+        return root
+    }
+}
